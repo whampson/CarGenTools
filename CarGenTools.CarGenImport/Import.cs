a8cae97d721802a921c9e5ba8ec92e6db57fe4ac
Wes Hampson <thehambone93@gmail.com>
2020-05-29 23:23:37 -0700

    Fixed a bug
    Fixed a bug causing some car generators to not spawn

--- CarGenTools.CarGenImport/Import.cs
+++ CarGenTools.CarGenImport/Import.cs
@@ -3,15 +3,17 @@ using GTASaveData.GTA3;
 using GTASaveData.Types.Interfaces;
 using GTASaveData.VC;
 using System;
+using System.Collections.Generic;
 using System.Linq;
-using GTA3CarGens = GTASaveData.GTA3.CarGeneratorData;
-using VCCarGens = GTASaveData.VC.CarGeneratorData;
+using GTA3CarGenBlock = GTASaveData.GTA3.CarGeneratorData;
+using VCCarGenBlock = GTASaveData.VC.CarGeneratorData;
 
 namespace CarGenTools.CarGenImport
 {
     public class Import : Tool<Options>
     {
         private bool SetTitle { get; }
+        private int MaxCapacity => MaxCapacityMap[Options.Game];
 
         public Import(Options options)
             : base(options)
@@ -21,12 +23,14 @@ namespace CarGenTools.CarGenImport
 
         private void UpdateCarGenMetadata(ICarGeneratorData carGens)
         {
-            carGens.NumberOfValidCarGenerators = 0;
+            carGens.NumberOfCarGenerators = MaxCapacity;
             carGens.NumberOfEnabledCarGenerators = 0;
             foreach (ICarGenerator g in carGens.CarGenerators)
             {
-                if (g.Model != 0) carGens.NumberOfValidCarGenerators++;
-                if (g.Model != 0 && g.Enabled) carGens.NumberOfEnabledCarGenerators++;
+                if (g.Model != 0 && g.Enabled)
+                {
+                    carGens.NumberOfEnabledCarGenerators++;
+                }
             }
         }
 
@@ -39,19 +43,19 @@ namespace CarGenTools.CarGenImport
 
             if (!TryOpenSaveData(Options.SaveDataFile, out TSaveData save)) return;
             if (!TryReadTextFile(Options.CarGenFile, out string json)) return;
-            if (!TryDeserializeCarGenData(json, out ICarGeneratorData newCarGenData, out int maxCount)) return;
+            if (!TryDeserializeCarGenData(json, out ICarGeneratorData newCarGenData)) return;
 
             int numCarGens = newCarGenData.CarGenerators.Count();
-            if (numCarGens > maxCount)
+            if (numCarGens > MaxCapacity)
             {
-                Log.Info($"Warning: Car generator limit exceeded! A maximum of {maxCount} car generators will be imported.");
+                Log.Info($"Warning: Car generator limit exceeded! A maximum of {MaxCapacity} car generators will be imported.");
             }
 
             ISaveData isave = (save as ISaveData);
             if (Options.Replace)
             {
                 isave.CarGenerators = newCarGenData;
-                Log.Info($"Replaced {maxCount} car generators.");
+                Log.Info($"Replaced {MaxCapacity} car generators.");
             }
             else
             {
@@ -100,26 +104,23 @@ namespace CarGenTools.CarGenImport
             Result = ExitCode.Success;
         }
 
-        private bool TryDeserializeCarGenData(string json, out ICarGeneratorData data, out int maxCount)
+        private bool TryDeserializeCarGenData(string json, out ICarGeneratorData data)
         {
             bool result;
             data = null;
-            maxCount = 0;
 
             switch (Options.Game)
             {
                 case Game.GTA3:
                 {
-                    result = TryDeserializeJson(json, out GTA3CarGens carGens);
+                    result = TryDeserializeJson(json, out GTA3CarGenBlock carGens);
                     data = carGens;
-                    maxCount = GTA3CarGens.Limits.MaxNumCarGenerators;
                     break;
                 }
                 case Game.VC:
                 {
-                    result = TryDeserializeJson(json, out VCCarGens carGens);
+                    result = TryDeserializeJson(json, out VCCarGenBlock carGens);
                     data = carGens;
-                    maxCount = VCCarGens.Limits.MaxNumCarGenerators;
                     break;
                 }
                 default:
@@ -150,5 +151,11 @@ namespace CarGenTools.CarGenImport
                 }
             }
         }
+
+        private static readonly Dictionary<Game, int> MaxCapacityMap = new Dictionary<Game, int>()
+        {
+            { Game.GTA3, GTA3CarGenBlock.Limits.MaxNumCarGenerators },
+            { Game.VC, VCCarGenBlock.Limits.MaxNumCarGenerators },
+        };
     }
 }
